0eefccb31962f37a10148a74af7cb1eab735e530
Taofeekolamide <taofeekolamide55@gmail.com>
2026-02-04 11:16:03 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,8 +24,8 @@ function Header() {
 
             <div className="header">
                 <input type="text" placeholder="Search for a movie..." value={search} onChange={(e) => setSearch(e.target.value)} />
-                {search && <GiCancel color="#6b6b6b" fontSize="40px" onClick={clearInput} />}
-                {search && <BiSearch color="#6b6b6b" fontSize="40px" onClick={SearchLink} />}
+                {search && <GiCancel color="#6b6b6b" fontSize="35px" onClick={clearInput} />}
+                {search && <BiSearch color="#6b6b6b" fontSize="35px" onClick={SearchLink} />}
             </div>
             {!toggleNav ? <FaBars className="menu" onClick={() => setToggleNav(!toggleNav)} /> : <HiX className="menu" onClick={() => setToggleNav(!toggleNav)} />}
 
